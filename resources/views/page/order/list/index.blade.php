dff480398dd3edf336f2ce8d1dedfaa312c5a3c2
Dean <bossgenius54@gmail.com>
2020-03-19 01:19:37 +0600

    dont show empty orders in list

--- resources/views/page/order/list/index.blade.php
+++ resources/views/page/order/list/index.blade.php
@@ -12,7 +12,7 @@
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">
-                    {{ $title }}  
+                    {{ $title }}
                     @can('create', App\Model\Order::class)
                         <div class="btn-group btn-group-sm pull-right">
                             <button type="button" class="btn btn-info btn-rounde dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -20,29 +20,29 @@
                             </button>
                             <div class="dropdown-menu">
                                 @foreach ($ar_type as $k=>$v)
-                                    <a href="{{ action('Order\CreateOrderController@getCreate', $k) }}" 
+                                    <a href="{{ action('Order\CreateOrderController@getCreate', $k) }}"
                                         class="dropdown-item " >
                                         Добавить "{{ $v }}"
                                     </a>
                                 @endforeach
-                            </div> 
+                            </div>
                         </div>
                     @endcan
                     @can('createForFiz', App\Model\Order::class)
-                        <a href="{{ action('Order\CreateFizOrderController@getCreate') }}" type="button" 
+                        <a href="{{ action('Order\CreateFizOrderController@getCreate') }}" type="button"
                             class="btn btn-sm btn-info btn-rounded pull-right" >
                             Добавить
                         </a>
                     @endcan
                     @can('createForCompanyClient', App\Model\Order::class)
-                        <a href="{{ action('Order\CreateSimpleCompanyOrderController@getCreate') }}" type="button" 
+                        <a href="{{ action('Order\CreateSimpleCompanyOrderController@getCreate') }}" type="button"
                             class="btn btn-sm btn-info btn-rounded pull-right" >
                             Добавить
                         </a>
                     @endcan
                 </h4>
             </div>
-            
+
             <table class="table  table-hover color-table muted-table" >
                 <thead>
                     <tr>
@@ -61,25 +61,27 @@
                 </thead>
                 <tbody>
                     @foreach ($items as $i)
-                        <tr class=" {{ $loop->index % 2 === 0 ? 'footable-odd'  : 'footable-even' }}" >
-                            <td>{{ $i->id }}</td>
-                            <td>{{ $i->is_retail ? 'Розница' : 'Оптовая' }}</td>
-                            <td>{{ isset($ar_type[$i->type_id]) ? $ar_type[$i->type_id] : 'не указано' }}</td>
-                            <td>{{ isset($ar_status[$i->status_id]) ? $ar_status[$i->status_id] : 'не указано' }}</td>
-                            <td>{{ isset($ar_branch[$i->branch_id]) ? $ar_branch[$i->branch_id] : 'не указано' }}</td>
-                            <td>{{ $i->getClient() ? $i->getClient()->name : 'не указано' }}</td>
-                            <td>{{ $i->total_sum }}</td>
-                            <td>{{ $i->prepay_sum }}</td>
-                            <td>{{ $i->relCreatedUser ? $i->relCreatedUser->name : 'не указано'  }}</td>
-                            <td>{{ $i->created_at }}</td>
-                            <td>
-                                @can('view', $i)
-                                    <a class="btn btn-info btn-sm" href="{{ action('Order\ViewController@getView', $i) }}">
-                                        Детально
-                                    </a>
-                                @endcan
-                            </td>
-                        </tr>
+                        @if($i->total_sum != null || $i->total_sum != '')
+                            <tr class=" {{ $loop->index % 2 === 0 ? 'footable-odd'  : 'footable-even' }}" >
+                                <td>{{ $i->id }}</td>
+                                <td>{{ $i->is_retail ? 'Розница' : 'Оптовая' }}</td>
+                                <td>{{ isset($ar_type[$i->type_id]) ? $ar_type[$i->type_id] : 'не указано' }}</td>
+                                <td>{{ isset($ar_status[$i->status_id]) ? $ar_status[$i->status_id] : 'не указано' }}</td>
+                                <td>{{ isset($ar_branch[$i->branch_id]) ? $ar_branch[$i->branch_id] : 'не указано' }}</td>
+                                <td>{{ $i->getClient() ? $i->getClient()->name : 'не указано' }}</td>
+                                <td>{{ $i->total_sum }}</td>
+                                <td>{{ $i->prepay_sum }}</td>
+                                <td>{{ $i->relCreatedUser ? $i->relCreatedUser->name : 'не указано'  }}</td>
+                                <td>{{ $i->created_at }}</td>
+                                <td>
+                                    @can('view', $i)
+                                        <a class="btn btn-info btn-sm" href="{{ action('Order\ViewController@getView', $i) }}">
+                                            Детально
+                                        </a>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
                 <tfoot>
