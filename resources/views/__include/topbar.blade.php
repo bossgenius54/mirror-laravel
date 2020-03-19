cebebdca5ba74ea3ed67efe5b0f3f69745628441
Dean <bossgenius54@gmail.com>
2020-03-19 20:12:03 +0600

    profile icon has changed

--- resources/views/__include/topbar.blade.php
+++ resources/views/__include/topbar.blade.php
@@ -1,6 +1,6 @@
 <header class="topbar">
     <nav class="navbar top-navbar navbar-expand-md navbar-light">
-    
+
         <div class="navbar-header">
             <a class="navbar-brand" href="/">
                 <b>
@@ -8,7 +8,7 @@
                 </b>
                 <span>
                     <img src="/assets/images/logo-text.png" alt="homepage" class="dark-logo" style="height: 40px;     width: 128px;" />
-                </span>  
+                </span>
             </a>
         </div>
         <div class="navbar-collapse">
@@ -20,27 +20,27 @@
                     @can('create', App\Model\Order::class)
 
                         @php
-                            $ar_type = App\Model\SysOrderType::pluck('name', 'id')->toArray();   
+                            $ar_type = App\Model\SysOrderType::pluck('name', 'id')->toArray();
                         @endphp
 
                         <div class="btn-group btn-group-sm">
-                            <button type="button" class="btn btn-info dropdown-toggle" 
-                                    data-toggle="dropdown" 
-                                    aria-haspopup="true" 
+                            <button type="button" class="btn btn-info dropdown-toggle"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
                                     aria-expanded="false"
                                     style="font-size: 15px;">
 
                                     <i class="mdi mdi-cart-plus"></i>
-                                    Новый заказ 
+                                    Новый заказ
                             </button>
                             <div class="dropdown-menu">
                                 @foreach ($ar_type as $k=>$v)
-                                    <a href="{{ action('Order\CreateOrderController@getCreate', $k) }}" 
+                                    <a href="{{ action('Order\CreateOrderController@getCreate', $k) }}"
                                         class="dropdown-item " >
                                         Добавить "{{ $v }}"
                                     </a>
                                 @endforeach
-                            </div> 
+                            </div>
                         </div>
                     @endcan
 
@@ -52,7 +52,7 @@
                 </li>
             </ul>
             <ul class="navbar-nav my-lg-0">
-                
+
                 <li class="nav-item dropdown">
                     <a  class="nav-link dropdown-toggle text-muted waves-effect waves-dark" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
                             href="" >
@@ -74,7 +74,7 @@
                             <li role="separator" class="divider"></li>
                             <li>
                                 <a href="{{ action('ProfileController@getIndex') }}">
-                                    <i class="fa fa-power-off"></i> Профиль
+                                    <i class="mdi mdi-settings"></i> Профиль
                                 </a>
                             </li>
                             <li>
