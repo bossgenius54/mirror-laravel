3c16a5b8ea268dd485d1b0037fb225616e609d3b
Dinmukhamed <bossgenius54@gmail.com>
2020-03-31 15:31:15 +0600

    company list fixed

--- app/ModelList/CompanyList.php
+++ app/ModelList/CompanyList.php
@@ -13,20 +13,20 @@ class CompanyList {
     static function get(Request $request){
         $el = new CompanyList();
         $el->start($request);
-        
+
         return  $el->getResult();
     }
 
 
     private function getItems(){
         $items = Company::where('id', '>', 0);
-        if (in_array($this->user->type_id, [SysUserType::DIRECTOR, SysUserType::MANAGER, SysUserType::ACCOUNTER])){
-            $user = $this->user;
-            $items->whereHas('relSeller', function($q) use ($user){
-                $q->where('company_id', $user->company_id);
-            });
-        }
-            
+//        if (in_array($this->user->type_id, [SysUserType::DIRECTOR, SysUserType::MANAGER, SysUserType::ACCOUNTER])){
+//            $user = $this->user;
+//            $items->whereHas('relSeller', function($q) use ($user){
+//                $q->where('company_id', $user->company_id);
+//            });
+//        }
+
         $this->items = $items;
     }
 
