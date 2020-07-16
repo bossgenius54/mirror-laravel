68a28f520971654f018c699dc9a9346adc153ba8
Dinmukhamed Tezekbayev <bossgenius54@gmail.com>
2020-07-16 16:30:53 +0600

    sale export fixed service filter

--- app/Exports/SalesReportExportView.php
+++ app/Exports/SalesReportExportView.php
@@ -55,20 +55,15 @@ class SalesReportExportView implements FromView
         }, 'relCategory'])->get();
 
         $ar['s_items'] = CompanyService::where('company_id', $request->user()->company_id)
-                                        ->whereHas('relOrderService', function($query) use ($request) {
-                                            $query->whereBetween(DB::raw('DATE(created_at)'), array($request->created_at_first, $request->created_at_second) )
-                                                ->whereHas('relOrder', function($query) use ($request) {
+                                        ->with(['relOrderService' => function($q) use ($request){
+                                            $q->whereBetween(DB::raw('DATE(created_at)'), array($request->created_at_first, $request->created_at_second) )
+                                                ->with(['relOrder' => function($query) use ($request) {
+                                                if($request->branch_id && $request->branch_id > 0){
+                                                    $query->where('branch_id', $request->branch_id);
+                                                }
 
-                                                    if($request->branch_id && $request->branch_id > 0){
-                                                        $query->where('branch_id', $request->branch_id);
-                                                    }
-
-                                                    $query->orderBy('branch_id', 'DESC');
-                                                });
-
-                                        })
-                                        ->with(['relOrderService' => function($q) {
-                                            $q->with('relOrder');
+                                                $query->orderBy('branch_id', 'DESC');
+                                            }]);
                                         }])->get();
 
         return view('page.report.sale.excel', $ar);
