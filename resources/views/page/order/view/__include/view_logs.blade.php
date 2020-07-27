e1e2f60856563569e13d76a532fd2f1a2572bbe7
Dinmukhamed Tezekbayev <bossgenius54@gmail.com>
2020-07-27 18:38:54 +0600

    fixed style in order logs

--- resources/views/page/order/view/__include/view_logs.blade.php
+++ resources/views/page/order/view/__include/view_logs.blade.php
@@ -12,7 +12,7 @@
             @php
                 $alert_type = $log->type_id == $delete_service ? 'alert-warning' : ($log->type_id == $delete_position ? 'alert-warning' : 'alert-success') ;
             @endphp
-            <div class="alert {{ $alert_type }}" role="alert">
+            <div class="alert {{ $alert_type }}" role="alert" style="font-size: 11px; font-height:11px;">
                 <b style="font-weight: bold;">{{ $log->created_at }}</b>
                 <hr/>
                 Пользователь <b style="font-weight: bold;">{{ $log->relCreatedUser->name }} ({{ $log->relCreatedUser->getClearTypeName() }})</b> {{ $log->relOrderLogType->name }}
